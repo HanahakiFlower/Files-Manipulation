a585ccf5468cf40b19c0e92a31d52e3d642001a4
Yumi Takahashi <roberta.takahashi@aluno.faculdadeimpacta.com.br>
2021-06-03 20:09:13 -0300

    starting

--- scr/src/mainclass.java
+++ scr/src/mainclass.java
@@ -1,5 +1,22 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class mainclass {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        File filePath = new File("C:\\Users\\rober\\OneDrive\\Documentos\\FACULDADE\\original-file.csv");
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line = br.readLine();
+            while (line != null) {
+                System.out.println(line.split(","));
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
