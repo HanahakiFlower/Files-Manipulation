42aadbaf4775f833e31584865b2543aafdd6d361
Yumi Takahashi <roberta.takahashi@aluno.faculdadeimpacta.com.br>
2021-06-04 10:37:53 -0300

    testing the data in the files

--- scr/src/mainclass.java
+++ scr/src/mainclass.java
@@ -1,19 +1,19 @@
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class mainclass {
     public static void main(String[] args) {
-
-        File filePath = new File("C:\\Users\\rober\\OneDrive\\Documentos\\FACULDADE\\original-file.csv");
-
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+        String path = "C:\\Users\\rober\\OneDrive\\Documentos\\FACULDADE\\original-file.csv";
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine();
             while (line != null) {
-                System.out.println(line.split(","));
+                String[] item = line.split(",");
                 line = br.readLine();
+                System.out.println("Produto: " + item[0] );
+                System.out.println("Preço: " + item[1] );
+                System.out.println("Quantidade: " + item[2] );
+                System.out.println("Total: " + Double.parseDouble(item[1]) * Double.parseDouble(item[2]));
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
