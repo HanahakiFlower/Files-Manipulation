45b53ed30272334a2670d8140306b2478343403b
Yumi Takahashi <roberta.takahashi@aluno.faculdadeimpacta.com.br>
2021-06-06 22:38:53 -0300

    ta bugado mas eh isto rs vambora

--- scr/src/mainclass.java
+++ scr/src/mainclass.java
@@ -1,22 +1,59 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class mainclass {
     public static void main(String[] args) {
-        String path = "C:\\Users\\rober\\OneDrive\\Documentos\\FACULDADE\\original-file.csv";
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the file path: ");
+        String path = sc.nextLine();
+        File file = new File(path);
+        readNcreate_file(path, file);
+
+    }
+
+    public static void readNcreate_file(String path, File file) {
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine();
             while (line != null) {
                 String[] item = line.split(",");
+                write_file(file, item[0], Double.parseDouble(item[1]) * Double.parseDouble(item[2]));
                 line = br.readLine();
-                System.out.println("Produto: " + item[0] );
-                System.out.println("Preço: " + item[1] );
-                System.out.println("Quantidade: " + item[2] );
-                System.out.println("Total: " + Double.parseDouble(item[1]) * Double.parseDouble(item[2]));
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void write_file(File file, String product, Double price) {
+        String new_path = file.getParent() + "\\summary.csv";
+        File check_file = new File(new_path);
+        if (check_file.exists()) {
+            String[] lines = new String[] { product, ", ", price.toString() + "\n" };
+
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(new_path, true))) {
+                for (String line : lines) {
+                    bw.write(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            String[] lines = new String[] { product, ", ", price.toString() };
+
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(new_path))) {
+                for (String line : lines) {
+                    bw.write(line);
+                }
+                bw.newLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
